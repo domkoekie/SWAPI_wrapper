d77f17e62e4da20efa4689581509f700733a5346
ktln-jnt <joyntkatelyn@gmail.com>
2025-09-25 20:46:49 +0200

    comments

--- authentication.py
+++ authentication.py
@@ -8,16 +8,13 @@ def init_jwt(app):
     jwt = JWTManager(app)
     return jwt
 
-# Login endpoint
 @auth_bp.route("/login", methods=["POST"])
 def login():
     username = request.json.get("username", None)
     password = request.json.get("password", None)
 
-    # Simple hardcoded check (replace with DB or service in real life)
     if username != "katelyn" or password != "swapi":
         return jsonify({"error": "Invalid credentials"}), 401
 
-    # Generate JWT token
     access_token = create_access_token(identity=username)
     return jsonify(access_token=access_token), 200
