17213c9bbfa5a6ee07f658ae6801aeefa82b8cb3
ktln-jnt <joyntkatelyn@gmail.com>
2025-09-26 13:03:44 +0200

    authentication changes new username + password

--- authentication.py
+++ authentication.py
@@ -4,7 +4,7 @@ from flask_jwt_extended import JWTManager, create_access_token, jwt_required
 auth_bp = Blueprint("auth", __name__) #blueprint for routes
 
 def init_jwt(app):
-    app.config["JWT_SECRET_KEY"] = "ilovesenwes"
+    app.config["JWT_SECRET_KEY"] = "ilovesenwes101"
     jwt = JWTManager(app)
     return jwt
 
@@ -14,7 +14,7 @@ def login():
     username = request.json.get("username", None)
     password = request.json.get("password", None)
 
-    if username != "katelyn" or password != "swapi": #hardcoded authentication values
+    if username != "graduate" or password != "ilovesenwes": #hardcoded authentication values
         return jsonify({"error": "Invalid credentials"}), 401 #error handling
 
     access_token = create_access_token(identity=username)
